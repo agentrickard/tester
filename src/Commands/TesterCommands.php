a8c56d1b63140c2d1a255395aa8a922d315a8d90
Ken Rickard <agentrickard@gmail.com>
2022-06-30 14:34:03 -0400

    Adds proper setup and teardown.

--- src/Commands/TesterCommands.php
+++ src/Commands/TesterCommands.php
@@ -6,6 +6,7 @@ use Drupal\tester\TesterPluginManager;
 use Drush\Commands\DrushCommands;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Extension\ModuleInstallerInterface;
 use Drupal\Core\State\StateInterface;
 use GuzzleHttp\Client;
 use GuzzleHttp\TransferStats;
@@ -24,6 +25,11 @@ class TesterCommands extends DrushCommands {
    * @var \Drupal\Core\Extension\ModuleHandlerInterface
    */
   protected $moduleHandler;
+
+  /**
+   * @var \Drupal\Core\Extension\ModuleInstallerInterface
+   */
+  protected $moduleInstaller;
 
   /**
    * @var \Drupal\Core\Config\ConfigFactoryInterface
@@ -47,6 +53,8 @@ class TesterCommands extends DrushCommands {
    *   The tester plugin manager.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler.
+   * @param \Drupal\Core\Extension\ModuleInstallerInterface $module_installer
+   *   The module installer.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory service.
    * @param \GuzzleHttp\Client $http_client
@@ -54,9 +62,10 @@ class TesterCommands extends DrushCommands {
    * @param \Drupal\Core\State\StateInterface $state
    *   The state interface
    */
-  public function __construct(TesterPluginManager $plugin_manager, ModuleHandlerInterface $module_handler, Client $http_client, ConfigFactoryInterface $config_factory, StateInterface $state) {
+  public function __construct(TesterPluginManager $plugin_manager, ModuleHandlerInterface $module_handler, ModuleInstallerInterface $module_installer, Client $http_client, ConfigFactoryInterface $config_factory, StateInterface $state) {
     $this->pluginManager = $plugin_manager;
     $this->moduleHandler = $module_handler;
+    $this->moduleInstaller = $module_installer;
     $this->configFactory = $config_factory;
     $this->httpClient = $http_client;
     $this->state = $state;
@@ -175,14 +184,48 @@ class TesterCommands extends DrushCommands {
    * set those back.
    */
   private function setUp() {
+    $state_change = [
+      'error_level' => NULL,
+      'dblog' => FALSE,
+    ];
 
+    // Set up error logging to highest level.
+    $config = $this->configFactory->getEditable('system.logging');
+    $error_level = $config->get('error_level');
+    if ($error_level !== "all") {
+      $config->set('error_level', 'all')->save();
+      $state_change['error_level'] = $error_level;
+    }
+
+    // Ensure dblog is enabled.
+    if (!$this->moduleHandler->moduleExists("dblog")) {
+      $this->moduleInstaller->install(['dblog']);
+      $state_change['dblog'] = TRUE;
+    }
+
+    // Set values in state.
+    $this->state->set('tester', $state_change);
   }
 
   /**
    * Tears down the crawler run by changing application state.
    */
   private function tearDown() {
+    $state_change = $this->state->get('tester');
 
+    // Reset error reporting.
+    if (!is_null($state_change['error_level'])) {
+      $config = $this->configFactory->getEditable('system.logging');
+      $config->set('error_level', $state_change['error_level'])->save();
+    }
+
+    // Reset dblog.
+    if ($state_change['dblog']) {
+      $this->moduleInstaller->uninstall(['dblog']);
+    }
+
+    // Clear state.
+    $this->state->delete('tester');
   }
 
 }
