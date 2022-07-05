b9f0b1c9dc4de7e701b38fa95a194a5ead0cf435
Ken Rickard <agentrickard@gmail.com>
2022-07-05 09:47:25 -0400

    Adds array_unique check.

--- src/Commands/TesterCommands.php
+++ src/Commands/TesterCommands.php
@@ -146,7 +146,7 @@ class TesterCommands extends DrushCommands {
       GLOBAL $base_url;
     }
 
-    $urls = $this->getUrls();
+    $urls = array_unique($this->getUrls());
 
     // We want to test 403 and 404 pages, so allow them.
     // See https://docs.guzzlephp.org/en/stable/request-options.html#http-errors
