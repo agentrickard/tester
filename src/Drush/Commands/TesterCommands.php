3a3dab7bda3e4144aad730bcad2b68d5e2ee1442
Ken Rickard <agentrickard@gmail.com>
2024-10-21 15:18:41 -0400

    Remove deprecated method

--- src/Drush/Commands/TesterCommands.php
+++ src/Drush/Commands/TesterCommands.php
@@ -206,7 +206,7 @@ class TesterCommands extends DrushCommands {
 
     if (is_null($options['test'])) {
       $select = $this->chooseOptions();
-      $options['test'] = $this->io()->choice($this->t('Select the tests to run'), $select);
+      $options['test'] = $this->io()->select($this->t('Select the tests to run'), $select);
     }
 
     if ($options['test'] === 'cancel') {
